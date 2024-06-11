b9b26cf9a4e6f30751db29ec49d02f14c30d0a92
Wolf <redenza@twitch.tv>
2024-06-11 10:03:44 -0400

    Initial Commit

---
+++ EventCountdown/EventCountdownApp.swift
@@ -0,0 +1,17 @@
+//
+//  EventCountdownApp.swift
+//  EventCountdown
+//
+//  Created by Reese, Wolf on 6/11/24.
+//
+
+import SwiftUI
+
+@main
+struct EventCountdownApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
